4842a7d8c7415de330bcf5c5bb2ab175cc889525
solvd <solvd@MacBook-Pro-de-solvd.local>
2023-05-25 13:36:57 -0300

    Camera captured and starting with recoginizing

---
+++ main.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+from decouple import config
+
+RTSP_URL = config('VIDEO_URL')
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
+
+if not cap.isOpened():
+    print('Cannot open RTSP stream')
+    exit(-1)
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('RTSP stream', frame)
+    # resizing for faster detection
+    frame = cv2.resize(frame, (640, 480))
+    # using a greyscale picture, also for faster detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    # detect people in the image
+    # returns the bounding boxes for the detected objects
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8, 8))
+
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+    for (xA, yA, xB, yB) in boxes:
+        # display the detected boxes in the colour picture
+        cv2.rectangle(frame, (xA, yA), (xB, yB),
+                      (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
